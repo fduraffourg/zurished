cc293c7a87b3e11f531d141d5b9775c9963a72ab
Florian Duraffourg <f.duraffourg@gmail.com>
2016-01-07 11:43:42 +0100

    FE: Add Signal to FolderView

--- frontend/FolderView.elm
+++ frontend/FolderView.elm
@@ -1,9 +1,11 @@
-module FolderView (Model, initModel, view) where
+module FolderView (Model, initModel, view, Action(Goto, NoOp)) where
 
 import Struct
 import String
 import List
 import Html exposing (..)
+import Html.Events exposing (onClick)
+import Signal
 
 -- MODEL
 
@@ -22,15 +24,20 @@ initModel path images =
     }
 
 
+type Action = Goto String | NoOp
+
 -- VIEW
 
-view : Model -> Html
-view model =
+view : Signal.Address Action -> Model -> Html
+view address model =
   let
-    imageList = ul [] (List.map imageToItem model.images)
+    imageList = ul [] (List.map (imageToItem address) model.images)
     folderList = ul []
       (String.split "/" model.path
-        |> List.map folderToItem)
+        |> List.foldl foldPath [("", "[root]")]
+        |> List.reverse
+        |> List.map (folderToItem address)
+      )
   in
     div []
       [ h1 [] [ text model.path ]
@@ -38,8 +45,24 @@ view model =
       , imageList
       ]
 
-imageToItem : Struct.Image -> Html
-imageToItem image = li [] [ text image.path ]
 
-folderToItem : String -> Html
-folderToItem name = li [] [ text name ]
+foldPath : String -> List (String, String) -> List (String, String)
+foldPath part prev =
+  let
+    (prevPath, _) = Maybe.withDefault ("", "") (List.head prev)
+    newItem = (prevPath ++ "/" ++ part, part)
+  in
+    newItem :: prev
+
+imageToItem : Signal.Address Action -> Struct.Image -> Html
+imageToItem address image =
+  let
+    path = image.path
+    name = path
+      |> String.split "/"
+      |> List.foldl (\a b -> a) ""
+  in
+    li [] [ text name ]
+
+folderToItem : Signal.Address Action -> (String, String) -> Html
+folderToItem address (path,name) = li [ onClick address (Goto path) ] [ text name ]
