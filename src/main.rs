40bf8f3320216df95bcf6a3ab09f0674dd705506
Florian Duraffourg <f.duraffourg@gmail.com>
2016-02-03 17:54:23 -0500

    HTTP Server with request routing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 extern crate iron;
 extern crate image;
-
-use iron::prelude::*;
-use iron::status;
+extern crate router;
 
 use std::fs;
 use std::fs::{DirEntry};
@@ -11,8 +9,27 @@ use std::path::Path;
 mod media;
 mod explorer;
 
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "index")))
+}
+
+
+fn list_albums(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "albums")))
+}
+
+
 fn main() {
-        Iron::new(|_: &mut Request| {
-            Ok(Response::with((status::Ok, "Hello world!")))
-                }).http("localhost:3000").unwrap();
+    let mut router = Router::new();
+
+    router.get("/albums", list_albums);
+    router.get("/", index);
+
+
+    Iron::new(router).http("localhost:3000").unwrap();
 }
